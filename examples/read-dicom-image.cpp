ae1aa2547e1ff847098b3ce297ca6b0dc4fb959d
Wilson Aguilar <52500170+waguilars@users.noreply.github.com>
2021-11-21 18:37:35 -0500

    :memo: docs: add example to read image dicom

---
+++ examples/read-dicom-image.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "DicomReader.h"
+#include "svm.h"
+
+using namespace std;
+
+// Read a dicom image and print its image array
+
+int main()
+{
+  DicomReader dicomObj("img/20586908_6c613a14b80a8591_MG_R_CC_ANON.dcm");
+  int **arr = dicomObj.getImageArray(12);
+  for (int i = 0; i < dicomObj.getHeight(); ++i)
+  {
+    for (int j = 0; j < dicomObj.getWidth(); ++j)
+    {
+      cout << arr[i][j] << " ";
+    }
+  }
+
+  return 0;
+}
