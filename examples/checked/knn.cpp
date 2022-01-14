a58e04c121df554e6667a580cf9412a0ce599e0f
Wilson Aguilar <52500170+waguilars@users.noreply.github.com>
2022-01-13 21:36:33 -0500

    :recycle: refactor: add new example for knn

--- examples/checked/knn.cpp
+++ examples/checked/knn.cpp
@@ -4,110 +4,48 @@
 #include <cmath>
 
 #include "dicom/DicomReader.h"
+#include "dicom/dicomutils.h"
+
 #include "knn/dataset.h"
 #include "knn/knn.h"
 #include "knn/knnUtils.h"
+#include "knn/Preprocessing.h"
 
 using namespace std;
 
-vector<vector<double>> readCSV(string filename) {
-    ifstream file(filename);
-    string line = "";
-    string delimiter = ",";
-
-    vector<vector<double>> data;
-
-    // Read label values and push to vector
-    while (getline(file, line))
-    {
-        size_t pos = 0;
-        string token;
-        vector<double>  dataValues;
-//        cout << line << endl;
-        while ((pos = line.find(delimiter)) != string::npos) {
-            token = line.substr(0, pos);
-            line.erase(0, pos + delimiter.length());
-            dataValues.push_back(stoi(token));
-        }
-        dataValues.push_back(stoi(line));
-        data.push_back(dataValues);
-    }
-    // Close the File
-    file.close();
-
-    return data;
-}
-
-
 int main()
 {
-    DicomReader dicomObj("/home/will/Projects/dicom-classifier/img/20586908_6c613a14b80a8591_MG_R_CC_ANON.dcm");
-    int rows = dicomObj.getHeight();
-    int cols = dicomObj.getWidth();
+    DicomReader dicomObj("/home/will/Projects/dicom-classifier/data/20586908_6c613a14b80a8591_MG_R_CC_ANON.dcm");
 
-    int **data = dicomObj.getImageArray(12);
-//    vector<vector<double>> data = dicomObj.getDoubleImageMatrix(12);
-//    vector<vector<double>> data= readCSV("/home/will/Downloads/dataset_withlabels.csv");
-      vector<double> dataLabels = readCSV("/home/will/Downloads/labels.csv")[0];
-    vector<vector<double>> testCSV = readCSV("/home/will/Downloads/test.csv");
+    vector<vector<double>> data= dicomObj.getDoubleImageMatrix(12);
+    vector<int> dataLabels = DicomUtils::genTargetValues(data, 2);
 
+    vector<int> testIdx = DicomUtils::genTestDataIdx(data, 50);
+    vector<int> testLabels = DicomUtils::getTestingLabels(dataLabels, testIdx);
+    vector<vector<double>> testData = DicomUtils::getTestingValues(data, testIdx);
 
-    // Parse to array
-    double *trainData = new double[rows*cols];
+    double *parsedTrainData = DicomUtils::parseKNNData(data);
+    int *parsedTrainLabel = DicomUtils::parseKNNLabels(dataLabels);
 
-    int k = 0;
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            trainData[k] = data[i][j];
-            k++;
-        }
-    }
-
-    int *trainLabels = new int[rows];
-    k = 0;
-    for (auto label : dataLabels) {
-        trainLabels[k] = label;
-        k++;
-    }
+    double *parsedTestData = DicomUtils::parseKNNData(testData);
+    int *parsedTestLabel = DicomUtils::parseKNNLabels(testLabels);
 
 
-
-    double *testData= new double[5*cols];
-    k=0;
-    for (auto row : testCSV) {
-        for (auto v : row) {
-            testData[k] = v;
-            k++;
-        }
-    }
-
-    int testLabels[] = {1,1,1,1,2};
-
+    int rows = data.size();
+    int cols = data[0].size();
 
     int numLabels = 2;
-    DatasetPointer dataset = makeDataset(rows, cols, numLabels, trainData, trainLabels);
-    DatasetPointer test = makeDataset(testCSV.size(), cols, numLabels, testData, testLabels);
+    DatasetPointer dataset = makeDataset(rows, cols, numLabels, parsedTrainData, parsedTrainLabel);
+    DatasetPointer test = makeDataset(testData.size(), cols, numLabels, parsedTestData, parsedTestLabel);
+
+    MatrixPointer meanData = MeanNormalize(dataset);
 
     KNN knn(dataset);
 
-
-//    double bestSuccessRate = 0;
-        int bestK = 0;
-//    for(int k=1; k<=10; k++) {
-//        printf("Trying K = %d ... ",k);
-//        KNNResults res = knn.run(k, test);
-//        double currentSuccess = res.top1Result().successRate();
-//        if (currentSuccess > bestSuccessRate) {
-//            bestSuccessRate = currentSuccess;
-//            bestK = k;
-//        }
-//        printf("%lf\n", currentSuccess);
-//    }
-//    printf("Best K: %d. Success rate in validation set: %lf\n", bestK, bestSuccessRate);
+    ApplyMeanNormalization(test, meanData);
 
 
-
-       bestK = 1;
+    int bestK = 2;
     KNNResults results = knn.run(bestK, test);
     cout << "Consolidating results" << endl;
 
@@ -115,24 +53,31 @@ int main()
     SingleExecutionResults top2 = results.topXResult(2);
     SingleExecutionResults top3 = results.topXResult(3);
 
-    printf("Success Rate: %lf, Rejection Rate: %lf\n", top1.successRate(), top1.rejectionRate());
-    printf("Top 2 Success Rate: %lf\n", top2.successRate());
-    printf("Top 3 Success Rate: %lf\n", top3.successRate());
-    printf("Confusion matrix:\n");
+    double successRate = top1.successRate();
 
 
+//    MatrixPointer predictions = results.getPredictions();
 
 
-    MatrixPointer predictions = results.getPredictions();
+   // Confusion matrix
+//    MatrixPointer confusionMatrix = results.getConfusionMatrix();
+//    for(size_t i = 0; i< confusionMatrix->rows; i++) {
+//        for(size_t j = 0; j< confusionMatrix->cols; j++) {
+//            if (j!=0) printf(",");
+//            printf("%d", (int)confusionMatrix->pos(i,j));
+//        }
+//        printf("\n");
+//    }
 
 
-    for (int i = 0; i < 5; ++i) {
-        cout << "Point: " << i << endl;
-        cout << "Original: " << testLabels[i]
-                << " Predicted: " << predictions->pos(i,0) << endl;
-    }
+    // Read Predicted values for test
+//    for (size_t i = 0; i < predictions->rows; ++i) {
+//        for (size_t j = 0; j < predictions->cols; ++j) {
+//            cout<< "Pred" << predictions->pos(i, j) << " ";
+//            cout<< "Pred" << predictions->pos(i, j) << " ";
+//        }
 
-
-
+//        cout << endl;
+//    }
 
 }
