e287f923ac0f3d2a093f46ce7b096f639b085029
Jefferson Romero <76419123+Jefferson-Romero@users.noreply.github.com>
2022-07-18 01:36:43 -0500

    Create gaussian.h

---
+++ Preprocesamiento/gaussian/gaussian.h
@@ -0,0 +1,6 @@
+#ifndef _GAUSSIANFILTER_H_
+#define _GAUSSIANFILTER_H_
+using namespace std;
+typedef int filtro;
+vector<vector<filtro>> Filtro_Gauss(vector<vector<filtro>> data,int SizeKernel);
+#endif
