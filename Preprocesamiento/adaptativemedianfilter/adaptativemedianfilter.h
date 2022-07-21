a3fed3abd142ccc995ef398722295e88dea4d44c
Daniel Mariño <76059405+dmarinoz@users.noreply.github.com>
2022-07-21 09:46:09 -0500

    Create adaptativemedianfilter.h

---
+++ Preprocesamiento/adaptativemedianfilter/adaptativemedianfilter.h
@@ -0,0 +1,12 @@
+#ifndef _ADAPTATIVEMEDIANFILTER_H_
+#define _ADAPTATIVEMEDIANFILTER_H_
+
+using namespace std;
+typedef int element;
+int minSize;
+int maxSize;
+
+vector<vector<element>> adaptivemedianfilter(vector<vector<element>> src, int minSize, int maxSize);
+
+
+#endif
