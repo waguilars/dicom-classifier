08cfeca38cd8ac933d3d51874182940c60ae0c12
Jhon Idrovo <76275379+jidrovo@users.noreply.github.com>
2022-07-30 17:48:19 -0500

    Update main.cpp

--- examples-Segmentacion/canny_libreria/main.cpp
+++ examples-Segmentacion/canny_libreria/main.cpp
@@ -10,15 +10,22 @@ using namespace std;
 
 int main()
 {
-         DicomReader dicomObj("/home/usuario/Descargas/imagendicom/dataset/MasaMicro2.dcm");
-         vector<vector<int>> data = dicomObj.getIntImageMatrix(12);
-         systemMetrics systemMetrics("canny");
-         systemMetrics.resetCounters();
-         data= canny(data,30,50);
-         systemMetrics.calculate();
-         systemMetrics.printMetrics();
-         cout <<"RAM: " <<getRamUsage()<<" kb"<<endl;
-         DicomReader::saveData(data,"/home/usuario/Documentos/metricas_resultados/matriz_resultanteejemplo.txt",",",0);
-         return 0;
+    //Lectura de la imagen con la libreria dicom
+    DicomReader dicomObj("/home/usuario/Descargas/imagendicom/dataset/MasaMicro4.dcm");
+    //Transforma la imagen dcm  en una matriz de tipo int
+    vector<vector<int>> vect = dicomObj.getIntImageMatrix(12);
+    //Metricas referente a la libreria dicomclsasifier
+    systemMetrics systemMetrics("Canny");
+    systemMetrics.resetCounters();
+    //metodo canny tiene como parametros de entrada(matriz de imagen(vect),valorminimo(30),valormaximo(50))
+    vect = canny(vect,30,50);
+    systemMetrics.calculate();
+    //Imprime las metricas en consola
+    systemMetrics.printMetrics();
+    //Imprimire en consola el total de ram utilizado en el programa
+    cout <<"RAM: " <<getRamUsage()<<" kb"<<endl;
+    //Guardra la matriz de imagen en un txt
+    dicomObj.saveData(vect,"/home/usuario/canny_paralelo1.csv",",",false);
+    return 0;
 
 }
